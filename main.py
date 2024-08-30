4dc80f6a79ca514a8f598a0bc50beda74f78a4bd
Jason Suter <jason.suter@hardinnorthern.org>
2024-08-30 08:58:31 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+#Ice Cream Price Calculator - www.101computing.net/ice-cream-price-calculator
+print("+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+")
+print("+                               +")
+print("+      The Ice Cream Shop       +")
+print("+            Welcome            +")
+print("+                               +")
+print("+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+")
+print("")
+
+container = input("What type of ice cream container would you like: cup or cone?")
+while container!="cup" and container!="cone":
+  print("Invalid answer please try again!")
+  container = input("What type of ice cream container would you like: cup or cone?")
+
+#Complete the code here...
